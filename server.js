90d660f3da9ae69672a9e4ef2a3f75e5abf4562e
Abhijit Dash <dashabhijeet650@gmail.com>
2026-05-12 21:31:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// DB Connection
+connectDB();
+
+// Routes
+app.use("/api/tasks", require("./routes/taskRoutes"));
+
+// Server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
